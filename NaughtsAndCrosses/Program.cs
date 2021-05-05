2643f7c87cdfad6615a95c97a50b3b06a73b7751
Jake Callcut <jakecallcut123@gmail.com>
2021-05-05 20:27:40 +0100

    final draft

--- NaughtsAndCrosses/Program.cs
+++ NaughtsAndCrosses/Program.cs
@@ -52,14 +52,15 @@ namespace NaughtsAndCrosses
                         Console.ReadKey();
                     }
                 }
+
                 else if (turnNumber % 2 == 1)
                 {
                     Console.WriteLine("Naughts turn");
                     DisplayBoard();
                     Console.WriteLine(" ");
                     Console.WriteLine("Please enter the coordinate where you would like to put your mark (e.g A1)");
-                    input = Console.ReadLine();
 
+                    input = Console.ReadLine();
                     char x = Convert.ToChar(input.Substring(0, 1));
                     char y = Convert.ToChar(input.Substring(1, 1));
                     AddToBoard(x, y, 'O');
@@ -90,6 +91,15 @@ namespace NaughtsAndCrosses
         }
 
         
+        public static char A1 = ' ';
+        public static char A2 = ' ';
+        public static char A3 = ' ';
+        public static char B1 = ' ';
+        public static char B2 = ' ';
+        public static char B3 = ' ';
+        public static char C1 = ' ';
+        public static char C2 = ' ';
+        public static char C3 = ' ';
 
         public static void DisplayBoard()
         {
@@ -100,16 +110,6 @@ namespace NaughtsAndCrosses
             Console.WriteLine("=|=|=|=|");
             Console.WriteLine("3|{0}|{1}|{2}|", A3, B3, C3);
         }
-
-        public static char A1 = ' ';
-        public static char A2 = ' ';
-        public static char A3 = ' ';
-        public static char B1 = ' ';
-        public static char B2 = ' ';
-        public static char B3 = ' ';
-        public static char C1 = ' ';
-        public static char C2 = ' ';
-        public static char C3 = ' ';
 
         public static bool HasWon(char _player)
         {
